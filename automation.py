3da593a8f1f9c4c23feb2237a681b81bca6f8c1e
Andrei <andrei.baditoiu@gmail.com>
2024-09-24 14:54:08 +0300

    Various modifications.

--- automation.py
+++ automation.py
@@ -2,14 +2,23 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
 service = Service('./chromedriver.exe')
 
 chrome_browser = webdriver.Chrome(service=service)
-chrome_browser.get('https://www.google.com')
+chrome_browser.get('https://demoqa.com/automation-practice-form')
 chrome_browser.maximize_window()
-time.sleep(10)
-chrome_browser.quit()
+time.sleep(2)
 
+assert 'DEMOQA' in chrome_browser.title
+submit_button = chrome_browser.find_element(By.CLASS_NAME, 'btn-primary')
+print(submit_button.get_attribute('innerHTML'))
 
-# print(chrome_browser)
+assert 'Submit' in chrome_browser.page_source
+user_message = chrome_browser.find_element(By.ID, 'firstName')
+user_message.clear()
+user_message.send_keys('I am cool!')
+chrome_browser.execute_script("arguments[0].scrollIntoView(true);", submit_button)
+submit_button.click()
+input("Press Enter to shot down...")
